300f775ce1cd96cba8dd78883b6c473675aea894
Evgeniya <popovaevg18@gmail.com>
2024-03-30 16:53:26 -0600

    Revert "JS code to TS"
    This reverts commit 17a7ea0e613da55f78e25afc9e8e86507ebcbf18.

--- src/index.ts
+++ src/index.ts
@@ -1,118 +1,115 @@
 class School {
-  directions: string[] = [];
+  directions = [];
 
-  addDirection(direction: string): void {
+  addDirection(direction) {
     this.directions.push(direction);
   }
 }
 
 class Direction {
-  levels: number[] = [];
-  _name: string;
+  levels = [];
 
-  get name(): string {
+  get name() {
     return this._name;
   }
 
-  constructor(name: string) {
+  constructor(name) {
     this._name = name;
   }
 
-  addLevel(level: number): void {
+  addLevel(level) {
     this.levels.push(level);
   }
 }
 
 class Level {
-  groups: never[] = [];
-  _program: string;
-  _name: string;
+  groups = [];
 
-  get name(): string {
-    return this._name;
-  }
-
-  get program(): string {
-    return this._program;
-  }
-
-  constructor(name: string, program: string) {
-    this._name = name;
+  constructor(name, program) {
+    this.name = name;
     this._program = program;
   }
 
-  addGroup(group: never): void {
+  get name() {
+    return this._name;
+  }
+
+  get program() {
+    return this._program;
+  }
+
+  addGroup(group) {
     this.groups.push(group);
   }
 }
 
 class Group {
-  _students: never[] = [];
-  directionName: string;
-  levelName: string;
+  _students = [];
 
-  get students(): never[] {
+  get students() {
     return this._students;
   }
 
-  constructor(directionName: string, levelName: string) {
+  constructor(directionName, levelName) {
     this.directionName = directionName;
     this.levelName = levelName;
   }
 
-  addStudent(student: never): void {
+  addStudent(student) {
     this._students.push(student);
   }
 
-  showPerformance(): never[] {
-    return this.students.toSorted((a: any, b: any) => b.getPerformanceRating() - a.getPerformanceRating());
+  showPerformance() {
+    const sortedStudents = this.students.toSorted(
+      (a, b) => b.getPerformanceRating() - a.getPerformanceRating()
+    );
+
+    return sortedStudents;
   }
 }
 
 class Student {
-  grades: any = {};
-  attendance: boolean[] = [];
-  firstName: string;
-  lastName: string;
-  birthYear: number;
+  grades = {};
+  attendance = [];
 
-  get fullName(): string {
-    return `${this.lastName} ${this.firstName}`;
-  }
-
-  set fullName(value: string) {
-    [this.lastName, this.firstName] = value.split(' ');
-  }
-
-  get age(): number {
-    return new Date().getFullYear() - this.birthYear;
-  }
-
-  constructor(firstName: string, lastName: string, birthYear: number) {
+  constructor(firstName, lastName, birthYear) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.birthYear = birthYear;
   }
 
-  setGrade(subject: string, grade: number): void {
+  get fullName() {
+    return `${this.lastName} ${this.firstName}`;
+  }
+
+  set fullName(value) {
+    [this.lastName, this.firstName] = value.split(" ");
+  }
+
+  get age() {
+    return new Date().getFullYear() - this.birthYear;
+  }
+
+  setGrade(subject, grade) {
     this.grades[subject] = grade;
   }
 
-  markAttendance(present: boolean): void {
+  markAttendance(present) {
     this.attendance.push(present);
   }
 
-  getPerformanceRating(): number {
-    const gradeValues: number[] = Object.values(this.grades);
-    const isZeroGradeValuesLength: unknown = gradeValues.length === 0;
+  getPerformanceRating() {
+    const gradeValues = Object.values(this.grades);
 
-    if (isZeroGradeValuesLength) return 0;
+    if (gradeValues.length === 0) return 0;
 
-    const averageGrade: number =
-      gradeValues.reduce((sum: number, grade: number) => sum + grade, 0) / gradeValues.length;
+    const averageGrade =
+      gradeValues.reduce((sum, grade) => sum + grade, 0) / gradeValues.length;
 
-    const attendancePercentage: number =
-      (this.attendance.filter((present: boolean) => present).length / this.attendance.length) * 100;
+    const attendancePercentage =
+      (this.attendance.filter((present) => present).length /
+        this.attendance.length) *
+      100;
 
     return (averageGrade + attendancePercentage) / 2;
   }
